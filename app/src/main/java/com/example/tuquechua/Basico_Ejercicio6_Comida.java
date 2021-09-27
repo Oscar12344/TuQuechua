50c9f33e487386bfc9fb0c120bb4a1ce7eb9d9d9
Oscar12344 <68335286+Oscar12344@users.noreply.github.com>
2021-09-27 11:11:37 -0500

    -----

---
+++ app/src/main/java/com/example/tuquechua/Basico_Ejercicio6_Comida.java
@@ -0,0 +1,14 @@
+package com.example.tuquechua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Basico_Ejercicio6_Comida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basico__ejercicio6__comida);
+    }
+}
